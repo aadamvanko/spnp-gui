04071fe324b3cb1fd946f6916872e5d045113c6b
Adam Vanko <aadamvanko@gmail.com>
2021-02-05 12:10:31 +0100

    Add basic example.

---
+++ src/main/java/cz/muni/fi/spnp/gui/PrimaryController.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.spnp.gui;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module cz.muni.fi {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires spnp.core.models;
+    requires spnp.core.transformators;
+
+    opens cz.muni.fi.spnp.gui to javafx.fxml;
+    exports cz.muni.fi.spnp.gui;
+}
