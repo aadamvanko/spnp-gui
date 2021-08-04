4168ec9feda310ab0afb08abff52d071593ce361
Adam Vanko <aadamvanko@gmail.com>
2021-08-04 14:02:37 +0200

    Fix - properties editor contained wrong class in map, add selecting of arc view when drag point is selected

--- src/main/java/cz/muni/fi/spnp/gui/components/propertieseditor/PropertiesComponent.java
+++ src/main/java/cz/muni/fi/spnp/gui/components/propertieseditor/PropertiesComponent.java
@@ -1,17 +1,13 @@
 package cz.muni.fi.spnp.gui.components.propertieseditor;
 
 import cz.muni.fi.spnp.gui.components.ApplicationComponent;
-import cz.muni.fi.spnp.gui.components.graph.elements.arc.StandardArcView;
 import cz.muni.fi.spnp.gui.components.propertieseditor.arc.InhibitorArcPropertiesEditor;
 import cz.muni.fi.spnp.gui.components.propertieseditor.arc.StandardArcPropertiesEditor;
 import cz.muni.fi.spnp.gui.components.propertieseditor.place.PlacePropertiesEditor;
 import cz.muni.fi.spnp.gui.components.propertieseditor.transition.ImmediateTransitionPropertiesEditor;
 import cz.muni.fi.spnp.gui.components.propertieseditor.transition.TimedTransitionPropertiesEditor;
 import cz.muni.fi.spnp.gui.model.Model;
-import cz.muni.fi.spnp.gui.viewmodel.DiagramViewModel;
-import cz.muni.fi.spnp.gui.viewmodel.ElementViewModel;
-import cz.muni.fi.spnp.gui.viewmodel.InhibitorArcViewModel;
-import cz.muni.fi.spnp.gui.viewmodel.PlaceViewModel;
+import cz.muni.fi.spnp.gui.viewmodel.*;
 import cz.muni.fi.spnp.gui.viewmodel.transition.immediate.ImmediateTransitionViewModel;
 import cz.muni.fi.spnp.gui.viewmodel.transition.timed.TimedTransitionViewModel;
 import javafx.beans.value.ObservableValue;
@@ -32,6 +28,7 @@ public class PropertiesComponent extends ApplicationComponent {
     private Class<?> currentType;
     private PropertiesEditor currentEditor;
     private final ListChangeListener<ElementViewModel> onSelectedChangedListener;
+    private DiagramViewModel diagramViewModel;
 
     public PropertiesComponent(Model model) {
         super(model);
@@ -39,7 +36,7 @@ public class PropertiesComponent extends ApplicationComponent {
         editors = new HashMap<>();
         editors.put(null, new PropertiesEditorPlaceholder());
         editors.put(PlaceViewModel.class, new PlacePropertiesEditor());
-        editors.put(StandardArcView.class, new StandardArcPropertiesEditor());
+        editors.put(StandardArcViewModel.class, new StandardArcPropertiesEditor());
         editors.put(InhibitorArcViewModel.class, new InhibitorArcPropertiesEditor());
         editors.put(ImmediateTransitionViewModel.class, new ImmediateTransitionPropertiesEditor());
         editors.put(TimedTransitionViewModel.class, new TimedTransitionPropertiesEditor());
@@ -55,6 +52,8 @@ public class PropertiesComponent extends ApplicationComponent {
     }
 
     private void onSelectedDiagramChanged(ObservableValue<? extends DiagramViewModel> observableValue, DiagramViewModel oldDiagram, DiagramViewModel newDiagram) {
+        diagramViewModel = newDiagram;
+
         if (oldDiagram != null) {
             oldDiagram.getSelected().removeListener(this.onSelectedChangedListener);
         }
@@ -74,29 +73,36 @@ public class PropertiesComponent extends ApplicationComponent {
     }
 
     private void setCorrectEditor(ObservableList<? extends ElementViewModel> selectedViewModels) {
-        return;
-//
-//        if (selectedViewModels.size() == 1) {
-//            var elementViewModel = selectedViewModels.get(0);
-//            if (elementViewModel.getClass().equals(currentType)) {
-//                currentEditor.unbindViewModel();
-//                currentEditor.bindViewModel(elementViewModel);
-//            } else {
-//                if (currentEditor != null) {
-//                    currentEditor.unbindViewModel();
-//                }
-//                currentEditor = editors.get(elementViewModel.getClass());
-//                currentEditor.bindViewModel(elementViewModel);
-//                vbox.getChildren().set(0, currentEditor.getRoot());
-//            }
-//            currentType = elementViewModel.getClass();
-//        } else {
-//            if (currentEditor != null) {
-//                currentEditor.unbindViewModel();
-//                currentEditor = null;
-//                currentType = null;
-//            }
-//        }
+        if (selectedViewModels.size() == 1) {
+            var elementViewModel = selectedViewModels.get(0);
+            if (elementViewModel instanceof DragPointViewModel) {
+                var dragPointViewModel = elementViewModel;
+                elementViewModel = diagramViewModel.getElements().stream()
+                        .filter(viewModel -> viewModel instanceof ArcViewModel)
+                        .filter(arcViewModel -> ((ArcViewModel) arcViewModel).getDragPoints().contains(dragPointViewModel))
+                        .findFirst().get();
+            }
+
+            if (elementViewModel.getClass().equals(currentType)) {
+                currentEditor.unbindViewModel();
+                currentEditor.bindViewModel(elementViewModel);
+            } else {
+                if (currentEditor != null) {
+                    currentEditor.unbindViewModel();
+                }
+                currentEditor = editors.get(elementViewModel.getClass());
+                currentEditor.bindViewModel(elementViewModel);
+                vbox.getChildren().set(0, currentEditor.getRoot());
+            }
+            currentType = elementViewModel.getClass();
+        } else {
+            if (currentEditor != null) {
+                currentEditor.unbindViewModel();
+                currentEditor = null;
+                currentType = null;
+            }
+            vbox.getChildren().set(0, editors.get(null).getRoot());
+        }
     }
 
 }
