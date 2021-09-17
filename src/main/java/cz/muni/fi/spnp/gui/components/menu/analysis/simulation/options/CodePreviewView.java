487dd8c8399da3941014000169c1c829b4f5d8d4
Adam Vanko <aadamvanko@gmail.com>
2021-09-17 11:07:09 +0200

    Add code preview and run view.

--- src/main/java/cz/muni/fi/spnp/gui/components/menu/analysis/simulation/options/CodePreviewView.java
+++ src/main/java/cz/muni/fi/spnp/gui/components/menu/analysis/simulation/options/CodePreviewView.java
@@ -1,13 +1,32 @@
 package cz.muni.fi.spnp.gui.components.menu.analysis.simulation.options;
 
+import cz.muni.fi.spnp.core.models.PetriNet;
+import cz.muni.fi.spnp.core.transformators.spnp.SPNPTransformator;
 import cz.muni.fi.spnp.gui.components.menu.view.UIWindowComponent;
+import cz.muni.fi.spnp.gui.mappers.DiagramMapper;
+import cz.muni.fi.spnp.gui.mappers.MappingException;
 import cz.muni.fi.spnp.gui.model.Model;
 import cz.muni.fi.spnp.gui.viewmodel.DiagramViewModel;
+import cz.muni.fi.spnp.gui.viewmodel.ViewModelCopyFactory;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.*;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class CodePreviewView extends UIWindowComponent {
 
     private final Model model;
     private final DiagramViewModel diagramViewModel;
+    private TextArea textArea;
+    private Button buttonRun;
 
     public CodePreviewView(Model model, DiagramViewModel diagramViewModel) {
         this.model = model;
@@ -17,7 +36,110 @@ public class CodePreviewView extends UIWindowComponent {
     }
 
     private void createView() {
+        var topPane = new VBox(new Label("Code:"));
+        topPane.setPadding(new Insets(5));
+
+        textArea = new TextArea();
+        var centerPane = new VBox(textArea);
+        centerPane.setPadding(new Insets(5));
+        HBox.setHgrow(textArea, Priority.ALWAYS);
+        VBox.setVgrow(textArea, Priority.ALWAYS);
+
+        var buttonGenerateCode = new Button("Generate code");
+        buttonGenerateCode.setOnAction(this::onButtonGenerateCodeHandler);
+
+        var buttonSaveCode = new Button("Export code");
+        buttonSaveCode.setOnAction(this::onButtonSaveCodeHandler);
+
+        buttonRun = new Button("Run");
+        buttonRun.setOnAction(this::onButtonRunHandler);
+
+        var paneSpacer = new Pane();
+        HBox.setHgrow(paneSpacer, Priority.ALWAYS);
+
+        var buttonClose = new Button("Close");
+        buttonClose.setOnAction(this::onButtonCloseHandler);
+
+        var buttonsPane = new HBox(buttonGenerateCode, buttonSaveCode, buttonRun, paneSpacer, buttonClose);
+        buttonsPane.setPadding(new Insets(5));
+        buttonsPane.setSpacing(5);
+
+        var borderPane = new BorderPane();
+        borderPane.setTop(topPane);
+        borderPane.setCenter(centerPane);
+        borderPane.setBottom(buttonsPane);
+
+        var scene = new Scene(borderPane);
+        stage.setScene(scene);
         stage.setTitle("Code preview");
+        stage.setWidth(1000);
+        stage.setHeight(600);
+
+        generateCode();
+    }
+
+    private void onButtonGenerateCodeHandler(ActionEvent actionEvent) {
+        generateCode();
+    }
+
+    private void generateCode() {
+        var acFinal = diagramViewModel.getFunctionByName("ac_final");
+        var viewModelCopyFactory = new ViewModelCopyFactory();
+        var acFinalCopy = viewModelCopyFactory.createCopy(acFinal);
+        var outputOptionsResult = new OutputOptionsResult();
+        model.getOutputOptions().forEach(outputOption -> {
+            outputOption.addToResult(outputOptionsResult, diagramViewModel);
+            outputOptionsResult.lines.add(System.lineSeparator());
+        });
+        var outputOptionsCode = String.join(System.lineSeparator(), outputOptionsResult.lines);
+        acFinal.bodyProperty().set(acFinal.getBody() + System.lineSeparator() + outputOptionsCode);
+
+        var diagramMapper = new DiagramMapper(model, diagramViewModel);
+        PetriNet petriNet = null;
+        try {
+            petriNet = diagramMapper.createPetriNet();
+        } catch (MappingException e) {
+            var errorMessage = String.format("Cannot generate CSPL source code because there is at least 1 error:%nElement: %s%nError: %s", e.getElementName(), e.getMessage().split(":")[1]);
+            textArea.setText(errorMessage);
+            textArea.setStyle("-fx-text-fill: red;");
+            buttonRun.setDisable(true);
+            return;
+        }
+        var spnpCode = diagramMapper.createSPNPCode();
+        var spnpOptions = diagramMapper.createSPNPOptions();
+        var transformator = new SPNPTransformator(spnpCode, spnpOptions);
+        var sourceCode = transformator.transform(petriNet);
+        textArea.setStyle("-fx-text-fill: black;");
+        textArea.setText(sourceCode);
+        buttonRun.setDisable(false);
+
+        acFinal.bodyProperty().set(acFinalCopy.getBody());
+    }
+
+    private void onButtonSaveCodeHandler(ActionEvent actionEvent) {
+        var fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose file");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Source Code Files", "*.c"),
+                new FileChooser.ExtensionFilter("All Files", "*.*")
+        );
+        File selectedFile = fileChooser.showSaveDialog(stage);
+        if (selectedFile != null) {
+            var sourceCode = textArea.getText();
+            try {
+                Files.writeString(selectedFile.getAbsoluteFile().toPath(), sourceCode);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void onButtonRunHandler(ActionEvent actionEvent) {
+        System.out.println("running");
+    }
+
+    private void onButtonCloseHandler(ActionEvent actionEvent) {
+        stage.close();
     }
 
 }
