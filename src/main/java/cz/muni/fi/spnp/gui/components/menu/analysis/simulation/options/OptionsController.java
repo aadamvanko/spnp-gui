f1f93cd75e88ec953c9ca9cf5b1967f99715468a
Adam Vanko <aadamvanko@gmail.com>
2021-09-22 17:15:10 +0200

    Remove unused classes.

--- src/main/java/cz/muni/fi/spnp/gui/components/menu/analysis/simulation/options/OptionsController.java
+++
@@ -1,7 +0,0 @@
-package cz.muni.fi.spnp.gui.components.menu.analysis.simulation.options;
-
-public class OptionsController {
-
-    public OptionsController() {
-    }
-}
--- src/main/java/cz/muni/fi/spnp/gui/components/menu/analysis/simulation/options/OptionsViewType.java
+++
@@ -1,6 +0,0 @@
-package cz.muni.fi.spnp.gui.components.menu.analysis.simulation.options;
-
-public enum OptionsViewType {
-    SIMULATION,
-    NUMERIC_ANALYSIS
-}
