f075a4ca88cc85b4ddf8034fb18dfcd48f29528e
Adam Vanko <aadamvanko@gmail.com>
2021-09-25 07:50:22 +0200

    Print lines immediately in SimulationTask instead of waiting for the process to end.

--- src/main/java/cz/muni/fi/spnp/gui/components/menu/analysis/simulation/options/SimulationTask.java
+++ src/main/java/cz/muni/fi/spnp/gui/components/menu/analysis/simulation/options/SimulationTask.java
@@ -10,14 +10,12 @@ import java.io.InputStreamReader;
 public class SimulationTask extends Task<Void> {
 
     private final Model model;
-    private final StringBuilder stringBuilder;
     private final String[] command;
     private Process process;
     private boolean isProcessDestroyed;
 
     public SimulationTask(Model model, String... command) {
         this.model = model;
-        this.stringBuilder = new StringBuilder();
         this.command = command;
     }
 
@@ -40,10 +38,7 @@ public class SimulationTask extends Task<Void> {
                 String line = in.readLine();
                 if (line == null)
                     break;
-                stringBuilder.append(line);
-                if (!line.isEmpty()) {
-                    stringBuilder.append(System.lineSeparator());
-                }
+                System.out.println(line);
             }
         } catch (Exception e) {
             System.err.println("Simulation/Analysis has failed due to the error:");
@@ -73,7 +68,6 @@ public class SimulationTask extends Task<Void> {
     protected void succeeded() {
         super.succeeded();
 
-        System.out.println(stringBuilder);
         System.out.println("Simulation/Analysis has finished.");
         destroyProcess();
     }
