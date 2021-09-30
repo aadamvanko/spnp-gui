47f040635195c9a537fe0c23a77db7a7cb229eed
Adam Vanko <aadamvanko@gmail.com>
2021-09-30 20:19:50 +0200

    Add crossing line on arc when the multiplicity is constant and differs from 1.

--- src/main/java/cz/muni/fi/spnp/gui/components/graph/elements/arc/ArcMultiplicityText.java
+++ src/main/java/cz/muni/fi/spnp/gui/components/graph/elements/arc/ArcMultiplicityText.java
@@ -2,21 +2,33 @@ package cz.muni.fi.spnp.gui.components.graph.elements.arc;
 
 import cz.muni.fi.spnp.gui.viewmodel.ArcViewModel;
 import javafx.geometry.Point2D;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 
 public class ArcMultiplicityText implements ArcMultiplicityVisual {
 
+    private static final double TEXT_OFFSET = 15;
+    private static final double CROSS_LINE_LENGTH = 8;
+    private static final double CROSS_LINE_ROTATION_OFFSET = 4;
+
+    private final Group container;
+    private final Line crossLine;
     private final Text textMultiplicity;
 
     public ArcMultiplicityText() {
+        crossLine = new Line();
+        crossLine.setStrokeWidth(1);
+
         textMultiplicity = new Text();
+
+        container = new Group(crossLine, textMultiplicity);
     }
 
     @Override
     public Node getRoot() {
-        return textMultiplicity;
+        return container;
     }
 
     @Override
@@ -26,17 +38,26 @@ public class ArcMultiplicityText implements ArcMultiplicityVisual {
         var midpoint = start.midpoint(end);
         var vector = end.subtract(start).normalize();
         var perpendicular = new Point2D(vector.getY(), -vector.getX());
-        double OFFSET = 10;
-        var pos = midpoint.add(perpendicular.multiply(OFFSET));
+        var pos = midpoint.add(perpendicular.multiply(TEXT_OFFSET));
         textMultiplicity.setX(pos.getX());
         textMultiplicity.setY(pos.getY());
         textMultiplicity.setX(textMultiplicity.getX() - textMultiplicity.getLayoutBounds().getWidth() / 2);
         textMultiplicity.setY(textMultiplicity.getY() + textMultiplicity.getLayoutBounds().getHeight() / 4);
+
+        var perpendicularNormalized = perpendicular.normalize();
+        var lineStart = midpoint.subtract(perpendicularNormalized.multiply(CROSS_LINE_LENGTH));
+        lineStart = lineStart.add(vector.multiply(CROSS_LINE_ROTATION_OFFSET));
+        var lineEnd = midpoint.add(perpendicularNormalized.multiply(CROSS_LINE_LENGTH));
+        lineEnd = lineEnd.subtract(vector.multiply(CROSS_LINE_ROTATION_OFFSET));
+        crossLine.setStartX(lineStart.getX());
+        crossLine.setStartY(lineStart.getY());
+        crossLine.setEndX(lineEnd.getX());
+        crossLine.setEndY(lineEnd.getY());
     }
 
     @Override
     public void setVisible(boolean visible) {
-        textMultiplicity.setVisible(visible);
+        container.setVisible(visible);
     }
 
     @Override
