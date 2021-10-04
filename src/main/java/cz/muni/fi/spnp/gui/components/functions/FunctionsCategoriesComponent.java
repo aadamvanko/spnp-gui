76ff635ca6a984465c95c543f0c2b67fe9fd1ede
Adam Vanko <aadamvanko@gmail.com>
2021-10-04 10:16:16 +0200

    Add double click on functions categories shows function view for editing or functions view.

--- src/main/java/cz/muni/fi/spnp/gui/components/functions/FunctionsCategoriesComponent.java
+++ src/main/java/cz/muni/fi/spnp/gui/components/functions/FunctionsCategoriesComponent.java
@@ -3,14 +3,22 @@ package cz.muni.fi.spnp.gui.components.functions;
 import cz.muni.fi.spnp.core.models.functions.FunctionType;
 import cz.muni.fi.spnp.gui.components.TreeViewContainer;
 import cz.muni.fi.spnp.gui.components.menu.view.functions.FunctionCategoryViewModel;
+import cz.muni.fi.spnp.gui.components.menu.view.functions.FunctionView;
 import cz.muni.fi.spnp.gui.components.menu.view.functions.FunctionViewModel;
+import cz.muni.fi.spnp.gui.components.menu.view.functions.FunctionsView;
+import cz.muni.fi.spnp.gui.components.menu.view.general.ItemViewMode;
 import cz.muni.fi.spnp.gui.model.Model;
 import cz.muni.fi.spnp.gui.viewmodel.DiagramViewModel;
 import cz.muni.fi.spnp.gui.viewmodel.DisplayableViewModel;
+import cz.muni.fi.spnp.gui.viewmodel.ViewModelCopyFactory;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ListChangeListener;
+import javafx.event.EventHandler;
+import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -87,6 +95,25 @@ public class FunctionsCategoriesComponent extends TreeViewContainer<FunctionView
         var functionTreeItem = createItem(functionViewModel);
         var categoryTreeItem = categories.get(functionViewModel.getFunctionType());
         categoryTreeItem.getChildren().add(functionTreeItem);
+    }
+
+    @Override
+    protected EventHandler<? super MouseEvent> getOnItemMouseClickHandler() {
+        return mouseEvent -> {
+            var sourceItem = ((TreeCell<DisplayableViewModel>) mouseEvent.getSource()).getItem();
+            if (sourceItem instanceof FunctionViewModel && mouseEvent.getButton() == MouseButton.PRIMARY && mouseEvent.getClickCount() == 2) {
+                var viewModelCopyFactory = new ViewModelCopyFactory();
+                var selectedFunction = (FunctionViewModel) sourceItem;
+                var copy = viewModelCopyFactory.createCopy(selectedFunction);
+                var functionView = new FunctionView(model.selectedDiagramProperty().get(), copy, selectedFunction, ItemViewMode.EDIT);
+                functionView.getStage().setTitle("Edit Function");
+                functionView.getStage().showAndWait();
+            } else if (mouseEvent.getButton() == MouseButton.PRIMARY && mouseEvent.getClickCount() == 2) {
+                var functionsView = new FunctionsView();
+                functionsView.bindDiagramViewModel(model.selectedDiagramProperty().get());
+                functionsView.getStage().showAndWait();
+            }
+        };
     }
 
 }
